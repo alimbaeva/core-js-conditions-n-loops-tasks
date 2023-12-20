a8358b28b8987264f038a305c25872f0f1395b68
Asel <asel.alimbaeva.kk@gmail.com>
2023-12-20 12:30:35 +0300

    feat: resolve getIndexOf

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -243,8 +243,15 @@ function isPalindrome(str) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let ind = null;
+  for (let i = 0; i < str.length; i += 1) {
+    if (ind === null && str[i] === letter) {
+      ind = i;
+      return i;
+    }
+  }
+  return -1;
 }
 
 /**
